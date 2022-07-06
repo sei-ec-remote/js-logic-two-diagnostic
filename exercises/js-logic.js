3ee251616e2bce4b1d50056018ce17cd062498c7
soungbin <soungbin941230@gmail.com>
2022-07-06 06:24:15 -0700

    Completed

--- exercises/js-logic.js
+++ exercises/js-logic.js
@@ -14,8 +14,10 @@ Write a function that takes in an array of 10 numbers and returns those numbers 
 
 Ex: If the array that was passed in was [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ] the return value should be '(012) 456-7890'. There should be a space between the area code and the start of the phone number.
 */
-const createPhoneNumber = () => {
-
+const createPhoneNumber = (numbers) => {
+      let phone_number = ""
+      phone_number = `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`
+      return phone_number
 }
 
 /*
@@ -25,8 +27,18 @@ Write a function that takes in a string and return the number of dupclicate valu
 
 Ex: If the string that was passed in was 'abc' the return value should be 0, also if 'aabbcc' is passed in 3 is returned. Also be aware that 'B' and 'b' are not equvalent. This is true for all capitalized values and there lower case counter part. 
 */
-const countTheDups = () => {
-
+const countTheDups = (str) => {
+      let dups = 0;
+      const chr_list = []
+      for( chr of str){
+            if(chr_list.includes(chr)){
+                  dups++
+            }
+            else{
+                  chr_list.push(chr)
+            }
+      }
+      return dups
 }
 
 /*
@@ -36,8 +48,22 @@ Write a function that takes in a string and return true or false if the string c
 
 Ex: If the string that was passed in was 'abc' the return value should be false, also if 'xoxo' is passed in true is returned. 
 */
-const boolsXOs = () => {
-
+const boolsXOs = (str) => {
+      let xs = 0
+      let ox = 0
+      for( chr of str){
+            if(chr == "x"){
+                  xs++
+            }
+            if(chr == "o"){
+                  ox++
+            }
+      }
+      if( xs == ox)
+      {
+            return true
+      }
+      return false
 }
 
 /* !!! DO NOT MODIFY ANYTHING BELOW HERE !!! */
