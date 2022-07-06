3323bf1402acc9e898a0160017cb70278bdb8cae
shaialoni <gitShai2022>
2022-07-06 06:34:06 -0700

    diagnostic complete

--- exercises/js-logic.js
+++ exercises/js-logic.js
@@ -14,8 +14,12 @@ Write a function that takes in an array of 10 numbers and returns those numbers 
 
 Ex: If the array that was passed in was [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ] the return value should be '(012) 456-7890'. There should be a space between the area code and the start of the phone number.
 */
-const createPhoneNumber = () => {
+const createPhoneNumber = (arr) => {
+      const area = arr.slice(0, 3).join('')
+	const prefix = arr.slice(3, 6).join('')
+	const lastFour = arr.slice(6, 10).join('')
 
+	return `(${area}) ${prefix}-${lastFour}`
 }
 
 /*
@@ -25,7 +29,15 @@ Write a function that takes in a string and return the number of dupclicate valu
 
 Ex: If the string that was passed in was 'abc' the return value should be 0, also if 'aabbcc' is passed in 3 is returned. Also be aware that 'B' and 'b' are not equvalent. This is true for all capitalized values and there lower case counter part. 
 */
-const countTheDups = () => {
+const countTheDups = (str) => {
+      const letters = str.split('')
+      let counter = 0
+      letters.map((letter, i, arr) => {
+            if (letter === arr[i-1]) {
+                  counter++
+            }
+      })
+      return counter
 
 }
 
@@ -36,8 +48,13 @@ Write a function that takes in a string and return true or false if the string c
 
 Ex: If the string that was passed in was 'abc' the return value should be false, also if 'xoxo' is passed in true is returned. 
 */
-const boolsXOs = () => {
-
+const boolsXOs = (str) => {
+      const arr = str.split('')
+      const xxx = arr.filter(i => i === 'x')
+      const ooo = arr.filter(i => i === 'o')
+      if (xxx.length === ooo.length) {
+            return true
+      }
 }
 
 /* !!! DO NOT MODIFY ANYTHING BELOW HERE !!! */
