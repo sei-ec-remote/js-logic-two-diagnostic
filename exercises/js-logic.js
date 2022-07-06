38a3d971d5f2efe4b1c96f754e0d15dd6f00b384
eshalali <eshal.a.ali@gmail.com>
2022-07-06 08:47:38 -0500

    Eshal Ali finsihed deliverable

--- exercises/js-logic.js
+++ exercises/js-logic.js
@@ -14,8 +14,12 @@ Write a function that takes in an array of 10 numbers and returns those numbers 
 
 Ex: If the array that was passed in was [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ] the return value should be '(012) 456-7890'. There should be a space between the area code and the start of the phone number.
 */
-const createPhoneNumber = () => {
-
+const createPhoneNumber = (arr) => {
+      let areaCode = arr.splice(0,3).join('')
+      let middle = arr.splice(0,3).join('')
+      let end = arr.splice(0,3).join('')
+      let last = String(arr[0])
+      return (`(${areaCode}) ${middle}-${end}${last}`)
 }
 
 /*
@@ -25,8 +29,14 @@ Write a function that takes in a string and return the number of dupclicate valu
 
 Ex: If the string that was passed in was 'abc' the return value should be 0, also if 'aabbcc' is passed in 3 is returned. Also be aware that 'B' and 'b' are not equvalent. This is true for all capitalized values and there lower case counter part. 
 */
-const countTheDups = () => {
-
+const countTheDups = (str) => {
+      let dupCount = 0
+      for (let i = 0; i < str.length; i++) {
+            if (str[i] === str[i+1]) {
+                  dupCount++
+            }
+      }
+      return dupCount
 }
 
 /*
@@ -36,8 +46,17 @@ Write a function that takes in a string and return true or false if the string c
 
 Ex: If the string that was passed in was 'abc' the return value should be false, also if 'xoxo' is passed in true is returned. 
 */
-const boolsXOs = () => {
-
+const boolsXOs = (str) => {
+      let xCount = 0
+      let oCount = 0
+      for (let i = 0; i < str.length; i++) {
+            if (str[i] === 'x') {
+                  xCount++
+            } else if (str[i] === 'o') {
+                  oCount++
+            }
+      }
+      return xCount === oCount
 }
 
 /* !!! DO NOT MODIFY ANYTHING BELOW HERE !!! */
