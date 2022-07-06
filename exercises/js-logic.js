a5a6384576a17c0cbef74d13a356d68dac1e8e1a
yuntian2424 <yuntian2424@gmail.com>
2022-07-06 09:29:06 -0400

    yuntian zheng-js logic two diagnostic complete

--- exercises/js-logic.js
+++ exercises/js-logic.js
@@ -14,10 +14,18 @@ Write a function that takes in an array of 10 numbers and returns those numbers 
 
 Ex: If the array that was passed in was [ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 ] the return value should be '(012) 456-7890'. There should be a space between the area code and the start of the phone number.
 */
-const createPhoneNumber = () => {
-
+const createPhoneNumber = (num) => {
+      let phone = "("
+      for(let i=0;i<num.length;i++){
+            if(i===3){
+                  phone+=") "
+            }else if(i===6){
+                  phone+="-"
+            }
+            phone+=num[i]
+      }
+      return phone
 }
-
 /*
 Question 2
 
@@ -25,8 +33,17 @@ Write a function that takes in a string and return the number of dupclicate valu
 
 Ex: If the string that was passed in was 'abc' the return value should be 0, also if 'aabbcc' is passed in 3 is returned. Also be aware that 'B' and 'b' are not equvalent. This is true for all capitalized values and there lower case counter part. 
 */
-const countTheDups = () => {
-
+const countTheDups = (str) => {
+      let dict ={}
+      let count = 0
+      for(let i=0;i<str.length;i++){
+            if(str[i] in dict){
+                  count++
+            }else{
+                  dict[str[i]]=str[i]
+            }
+      }
+      return count
 }
 
 /*
@@ -36,8 +53,23 @@ Write a function that takes in a string and return true or false if the string c
 
 Ex: If the string that was passed in was 'abc' the return value should be false, also if 'xoxo' is passed in true is returned. 
 */
-const boolsXOs = () => {
-
+const boolsXOs = (str) => {
+      let x = 0
+      let o = 0
+      for(let i=0;i<str.length;i++){
+            if(str[i]==="x"){
+                  x++
+            }else if(str[i]==="o"){
+                  o++
+            }else{
+                  return false
+            }
+      }
+      if(x==o){
+            return true
+      }else{
+            return false
+      }
 }
 
 /* !!! DO NOT MODIFY ANYTHING BELOW HERE !!! */
